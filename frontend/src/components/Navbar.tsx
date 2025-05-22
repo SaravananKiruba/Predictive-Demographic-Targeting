49c1ed4746c281e57ef5f98431d22c108db2cc2c
Saravanan kiruba S <kirubachetti@gmail.com>
2025-05-22 11:40:48 +0530

    FINAL

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -7,21 +7,12 @@ const Navbar: React.FC = () => {
   
   return (
     <Box as="nav" bg={bgColor} px={4} py={3} boxShadow="sm" position="sticky" top={0} zIndex={10}>
-      <Flex align="center" justify="space-between" maxW="1400px" mx="auto">
-        <Flex align="center">
-          <Image src="/logo192.png" alt="Logo" boxSize="40px" mr={3} />
+      <Flex align="center" justify="space-between" maxW="1400px" mx="auto">        <Flex align="center">
+          <Image src={require('../E2W_LOGO.png')} alt="E2W Logo" boxSize="40px" mr={3} />
           <Heading size="md" color="brand.600">Predictive Demographic Targeting</Heading>
         </Flex>
         
-        <HStack spacing={4}>
-          <Link to="/">
-            <Button variant="ghost">Home</Button>
-          </Link>
-          <Link to="#">
-            <Button variant="ghost">Documentation</Button>
-          </Link>
-          <Button colorScheme="blue">Help</Button>
-        </HStack>
+       
       </Flex>
     </Box>
   );
